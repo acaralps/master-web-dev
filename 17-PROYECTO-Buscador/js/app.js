ae2141213d28c294daeede35bbe88114f1eb04a0
acaralps <caralpsabel@gmail.com>
2023-12-15 19:31:28 +0100

    postfolio + img

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,7 +1,27 @@
-const resultado = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+
+const resultado = document.querySelector("#resultado");
+
+
 const max = new Date().getFullYear();
 const min = max - 10;
+
+const datosBusqueda = {
+  marca: '',
+  year:'',
+  minimo:'',
+  maximo:'',
+  puertas:'',
+  transmision:'',
+  color:''
+}
 
 //eventos
 document,
@@ -9,6 +29,11 @@ document,
     mostrarAutos(); //show cars
     llenarSelect(); //llenar combo de years
   });
+
+  //event listeners
+  marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+  })
 
 //functions
 function mostrarAutos() {
